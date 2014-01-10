67bd08f13c429a2c1dbd50c47f460cad44279beb
dryize <dryize@gmail.com>
2014-01-10 17:50:36 +0530

    fixed build errors

--- build.sh
+++ build.sh
@@ -8,15 +8,16 @@ export CROSS_COMPILE=~/android/toolchains/android-toolchain-eabi-4.7-2013.09/bin
 make ap33_android_defconfig
 make
 
-rm out/zImage
+rm ../out/zImage
 
-rm out/flashable/kernel/* -rf
-rm out/flashable/system/lib/modules/* -rf
+rm ../out/flashable/kernel.zip
+rm ../out/flashable/kernel/* -rf
+rm ../out/flashable/system/lib/modules/* -rf
 
-cp arch/arm/boot/zImage out/
-find . -name '*ko' -exec cp '{}' out/flashable/system/lib/modules/ \;
+cp arch/arm/boot/zImage ../out/
+find . -name '*ko' -exec cp '{}' ../out/flashable/system/lib/modules/ \;
 
-cd out/tools
+cd ../out/tools
 
 #./split_boot boot_add.img
 ./mkbootimg --kernel ../zImage --ramdisk ramdisk.cpio.gz --ramdiskaddr 0x0049C4F0  -o ../flashable/kernel/boot.img
